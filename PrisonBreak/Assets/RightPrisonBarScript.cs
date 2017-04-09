7321fc747be4d69b35e48ace3046156763a96765
farooqhassaan <hassaanfarooq247@gmail.com>
2017-04-09 17:58:46 +0300

    bars animation added

---
+++ PrisonBreak/Assets/RightPrisonBarScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RightPrisonBarScript : MonoBehaviour {
+	
+	public Image bars_2;
+	public Transform target;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		//target.position = new Vector3(141, 100, 0);
+		bars_2.transform.position = Vector3.MoveTowards(bars_2.transform.position, target.position, step);
+	}
+}
---
+++ PrisonBreak/Assets/leftPrisonBarScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class leftPrisonBarScript : MonoBehaviour {
+
+	public Image bars_1;
+	public Transform target;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		//target.position = new Vector3(141, 100, 0);
+		bars_1.transform.position = Vector3.MoveTowards(bars_1.transform.position, target.position, step);
+	}
+}
